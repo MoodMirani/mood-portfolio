868c51a7ac9f39d1c2cea9a7c6c123ecf7fb5b4a
Mahmoud Sherzad <47563174+MoodSherzad@users.noreply.github.com>
2023-10-27 10:29:05 +0200

    initial commit

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import useScroll from "@/lib/hooks/use-scroll";
+import { useSignInModal } from "./sign-in-modal";
+import UserDropdown from "./user-dropdown";
+import { Session } from "next-auth";
+
+export default function NavBar({ session }: { session: Session | null }) {
+  const scrolled = useScroll(50);
+
+  return (
+    <>
+      <div
+        className={`fixed top-0 flex w-full justify-center ${
+          scrolled ? "bg-white/30 backdrop-blur-xl" : "bg-white/0"
+        } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">
+          <Link href="/" className="flex items-center font-display text-2xl">
+            <Image
+              src="/logo.png"
+              alt="Precedent logo"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            ></Image>
+            <p>Mahmouds Signatur</p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
